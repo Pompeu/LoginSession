310f06c853b16294408093c63b5e7246b06ea2a4
pompeu <itacir@hotmail.com>
2016-05-19 01:14:25 -0300

    end login session

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,7 @@
 // file: controllers/login.js - created at 2015-11-28, 03:34
 const User = require('../models/index').User;
 function loginHandler(req, res) {
-  var user = req.body;
+  const user = req.body;
 
   User.findOne({email : user.email})
     .then(u => {
@@ -11,8 +11,8 @@ function loginHandler(req, res) {
       } else {
         loginError(res);
       }
-    } , err => {
-      res.json({err : err});
+    } , () => {
+      loginError(res);
     })
     .catch(() => { 
       loginError(res);
@@ -22,8 +22,8 @@ function loginHandler(req, res) {
 module.exports = exports = loginHandler;
 
 function loginError (res) {
-  return res.render('caduser',{ 
-    title : 'caduser',
+  return res.render('login',{ 
+    title : 'login',
     msg   : 'erro ao logar'
   });
 }
--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,36 @@
 // file: controllers/user.js - created at 2015-11-28, 03:23
 const User = require('../models/index').User;
-function userHandler(req, res) {
 
+function userHandler(req, res) {
   var user = req.body;
   if(user.password === user.password2){
     delete user.password2;
     User.create(user).then(user => {
-      if(user) res.json(user);
+      if (user) {
+        res.render('user-details', {
+          title : 'user details',
+          user : user
+        });
+      }
     }, err => {
-      console.log(err);
+      createError(res, err);
+    })
+    .catch(err => {
+      createError(res, err);
     });
 
-  }else {
-    res.render('caduser', {title : 'cadauser', err : 'mail not eq'});
+  } else {
+    createError(res);
   }
 }
+
 module.exports = exports = userHandler;
+
+function createError (res, err) {
+  res.render('caduser', {
+    title : 'cadauser',
+    err   : err ||'mail not eq'
+  });
+}
+
+
